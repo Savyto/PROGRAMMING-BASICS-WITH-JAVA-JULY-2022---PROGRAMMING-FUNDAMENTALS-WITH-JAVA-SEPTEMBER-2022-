cdcfed4090ef1d953bb02856076ed06bf4395dc2
Savy <68729958+Savyto@users.noreply.github.com>
2022-11-02 23:50:14 +0200

    Add files via upload

---
+++ P03SumEvenNumbers.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P03SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+          String[] inputArr = scanner.nextLine().split(" ");
+          int[] numsArr = new int[inputArr.length];
+
+          for (int i = 0; i < numsArr.length; i++) {
+              numsArr[i] = Integer.parseInt(inputArr[i]);
+          }
+
+
+        int evenSum = 0;
+        for (int i = 0; i < numsArr.length; i++) {
+            int currentElement = numsArr[i];
+            if (currentElement % 2 == 0) {
+                evenSum += currentElement;
+            }
+        }
+        System.out.println(evenSum);
+
+    }
+}
